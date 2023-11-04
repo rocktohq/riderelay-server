5eb2792c8ea3fb4ece9e368b27df9cf33f714ad1
rocktohq <itzmonir@gmail.com>
2023-11-05 04:07:44 +0600

    All Services API added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 
+const port = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
@@ -12,6 +14,53 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("RideRelay Server is running");
 });
+
+// * MongoDB COnfigurations
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@usermanagement.n4peacj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // * Collections
+    const serviceCollection = client.db("rideRelay").collection("services");
+
+    // * Routes
+    // Get Services
+
+    app.get("/api/v1/services", async (req, res) => {
+      try {
+        let query = {};
+        const cursor = serviceCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Listener
 
